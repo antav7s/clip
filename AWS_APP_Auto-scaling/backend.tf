aeb005af7b0199b3b5302cad9b7d625804803507
antav7s <86698388+antav7s@users.noreply.github.com>
2022-04-24 21:38:35 -0500

    Delete AWS_APP_Auto-scaling directory

--- AWS_APP_Auto-scaling/backend.tf
+++
@@ -1,8 +0,0 @@
-/* #Backup of state file written to terraform.tfstate.backup on S3 bucket.
-terraform {
-  backend "s3" {
-    bucket = "terra-state-riguerre"
-    key    = "terraform/backup_main"
-    region = "us-west-2"
-  }
-}*/
